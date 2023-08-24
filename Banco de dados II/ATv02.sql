6e8ec88c88abfcda4a51438f93f856bd3bc7cb75
Roger <94903084+rogerdapalma@users.noreply.github.com>
2023-08-24 09:13:26 -0300

    Update ATv02.sql

--- Banco de dados II/ATv02.sql
+++ Banco de dados II/ATv02.sql
@@ -71,20 +71,34 @@ GROUP BY CONCAT(E.FIRSTNAME, ' ', E.LASTNAME)
 GO
 
 	
-SELECT * from dbo.products;
+--Nome do produto, nome do fornecedor, nome da categoria, preço e estoque
+SELECT p.ProductName, s.CompanyName, c.CategoryName, p.UnitPrice, P.UnitsInStock 
 FROM Products p
 JOIN Suppliers s on p.SupplierID = s.SupplierID
 JOIN Categories c ON p.CategoryID = c.CategoryID;
 
+--Ocultar produtos com estoque zerado e descontinuados
 SELECT p.ProductName, p.UnitsInStock
 FROM Products p 
 WHERE p.UnitsInStock > 0 AND p.Discontinued = 0;
 
+--Listar produtos com estoque zerado ou descontinuados
 SELECT p.ProductName, p.UnitsInStock
 FROM Products p 
 WHERE p.UnitsInStock > 0 OR p.Discontinued = 1;
 
+--Nome do vendedor e a quantidade de vendas que ele possui
 SELECT e.FirstName, COUNT(O.OrderID) 
 FROM Employees e
 LEFT JOIN Orders o ON e.EmployeeID = o.EmployeeID
 GROUP BY e.FirstName;
+
+--Nome do vendedor e quantidade de territórios aos quais ele está vinculado
+SELECT e.FirstName, COUNT(et.TerritoryID) AS TerritoryCount
+FROM Employees e
+LEFT JOIN EmployeeTerritories et ON e.EmployeeID = et.EmployeeID
+GROUP BY e.FirstName;
+
+--Vendas ordenadas por valor total, do maior ao menor
+
+--Vendas em que o produto foi vendido mais barato do que o valor de compra
