4a1f5b4fa3487021768b5d93eccfa17dbd7eb52b
Roger <94903084+rogerdapalma@users.noreply.github.com>
2023-11-03 10:27:50 -0300

    Update Gerenciamento de Acessos SQL Server.sql

--- Banco de dados II/Nota 2/Gerenciamento de Acessos SQL Server.sql
+++ Banco de dados II/Nota 2/Gerenciamento de Acessos SQL Server.sql
@@ -38,7 +38,7 @@ SELECT name,
 FROM sys.sql_logins
 
 
---Instancias de conex„o 
+--Instancias de conex√£o 
 SELECT * FROM sys.sysprocesses
 WHERE loginame = 'Juca';
 
@@ -61,7 +61,7 @@ SELECT * FROM Disciplina;
 --Dar acesso ao Juca
 GRANT SELECT ON Disciplina TO Juca; 
 GO
--- Criar funÁ„o
+-- Criar fun√ß√£o
 CREATE FUNCTION fncDisciplina(@id int)
 RETURNS TABLE
 AS
@@ -70,10 +70,10 @@ RETURN
     SELECT * FROM Disciplina WHERE ID = @id
 );
 GO
--- Verificar funcionamento da FunÁ„o 
+-- Verificar funcionamento da Fun√ß√£o 
 SELECT * FROM fncDisciplina(3);
 
--- Dando acesso a execuÁ„o da funÁ„o 
+-- Dando acesso a execu√ß√£o da fun√ß√£o 
 GRANT SELECT ON fncDisciplina TO Juca;
 
 -- Criando uma View 
@@ -132,7 +132,7 @@ CREATE TABLE Instituicao
 
 --Preenchendo a tabela
 INSERT INTO Instituicao (nome,cod)
-SELECT 'Nome InstituiÁ„o',123
+SELECT 'Nome Institui√ß√£o',123
 GO 20
 
 DENY UPDATE TO Juca;
@@ -148,3 +148,16 @@ END
 GO
 
 EXEC sp_Disciplina_05;
+
+
+--Query para retornar as permi√ß~pes que sao dadas 
+SELECT	state_desc, prmsn.permission_name as [Permission], sp.type_desc, sp.name,
+		grantor_principal.name AS [Grantor], grantee_principal.name as [Grantee]
+FROM sys.all_objects AS sp
+	INNER JOIN sys.database_permissions AS prmsn 
+	ON prmsn.major_id = sp.object_id AND prmsn.minor_id=0 AND prmsn.class = 1
+	INNER JOIN sys.database_principals AS grantor_principal
+	ON grantor_principal.principal_id = prmsn.grantor_principal_id
+	INNER JOIN sys.database_principals AS grantee_principal 
+	ON grantee_principal.principal_id = prmsn.grantee_principal_id
+WHERE grantee_principal.name = 'Juca'
